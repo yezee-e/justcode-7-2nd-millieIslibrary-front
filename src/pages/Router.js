8b528b57be56d79da65aa458368575ee8a5ce6af
yeji <yejipooh@gmail.com>
2022-11-16 13:27:09 +0900

    2차 conflict 수정

--- src/pages/Router.js
+++ src/pages/Router.js
@@ -1,39 +1,21 @@
 import React from 'react';
-<<<<<<< HEAD:src/pages/Router.js
 import { Route, Routes } from 'react-router-dom';
 import App from '../App';
-=======
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Findpw from './Findpw/Findpw';
->>>>>>> origin/Develop:src/pages/Router.jsx
-
-import Today from './Home/Today';
 import Login from './Login/Login';
 import Signup from './Signup/Signup';
 import BookDetail from './BookDetail/BookDetail';
+import Home from './Home/Home';
+import Findpw from './Findpw/Findpw';
 
 function Router() {
   return (
-<<<<<<< HEAD:src/pages/Router.js
     <Routes>
-      <Route path="/" element={<App />} />
-
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/search" element={<Signup />} />
-      <Route path="/myBook" element={<Signup />} />
+      <Route path="/findpw" element={<Findpw />} />
+      <Route path="/bookDetail" element={<BookDetail />} />
     </Routes>
-=======
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/findpw" element={<Findpw />} />
-        <Route path="/bookDetail" element={<BookDetail />} />
-      </Routes>
-    </BrowserRouter>
->>>>>>> origin/Develop:src/pages/Router.jsx
   );
 }
 

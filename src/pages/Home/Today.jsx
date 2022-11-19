e7f1b396e83a4dada38a740a2d9173178fe50737
yeji <yejipooh@gmail.com>
2022-11-20 01:29:59 +0900

    버튼 카테고리

--- src/pages/Home/Today.jsx
+++ src/pages/Home/Today.jsx
@@ -1,47 +1,128 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Carousel, Col, Container, Row } from 'react-bootstrap';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import DragCarousel from '../../components/DragCarousel /DragCarousel';
 import './Today.scss';
 
 function Today() {
-  const bookCategory = ['트렌드', '라이프', '힐링', '지적교양', '소설'];
+  const [recommend, setRecommend] = useState([]);
+  const [query, setQuery] = useSearchParams('');
+  const limitNum = 6;
+  let limit = query.get('limit');
+  let btn = query.get('categoryName');
+  console.log('쿼리값은?: ', btn);
+
+  const getCategory = () => {
+    axios
+      .get(`http://localhost:3004/books?categoryName=${btn}&limit=${limit}`)
+      .then(res => {
+        setRecommend(res.data);
+      })
+      .catch(() => '로딩실패');
+    query.set('limit', limitNum);
+    // setQuery(query);
+  };
+
+  useEffect(() => {
+    getCategory();
+  }, [limit, btn]);
+
+  const navigate = useNavigate();
+  const bookType = btn => {
+    navigate(`/Home?categoryName=${btn}`);
+  };
+
+  const bookCategory = [
+    '예술/대중문화',
+    '어린이',
+    '소설/시/희곡',
+    '인문학',
+    '자기계발',
+  ];
 
   return (
     <div className="today-area">
       <Carousel>
         <Carousel.Item className="inner">
-          <img className="d-block w-100" src="img/7.png" alt="First slide" />
+          <img
+            className="d-block w-100 car-img"
+            src="img/7.png"
+            alt="First slide"
+          />
           <Carousel.Caption className="inner-text">
-            <div>앞서가는 당신을 위한 </div>
-            <div>트렌드 필독서</div>
-            <p>2023년, 이제는 준비해야할 때!</p>
+            <div>배움에는 끝이 없다 </div>
+            <div>인생은 도전이니까</div>
+            <p>진짜 나를 위한 공부를 시작해봐요!</p>
+            <div>
+              <img
+                src="img/find.png"
+                alt="carousel-img"
+                className="inner-img"
+              />
+            </div>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item className="inner">
-          <img className="d-block w-100" src="img/3.png" alt="Second slide" />
+          <img
+            className="d-block w-100 car-img"
+            src="img/3.png"
+            alt="Second slide"
+          />
 
           <Carousel.Caption className="inner-text">
-            <div>조상님들 나 빼고 </div>
-            <div>재밌는거 보셨네</div>
-            <p>만화로 보는 고전 소설 속 사랑과 욕망</p>
+            <div>드디어 수능 끝 </div>
+            <div>고생한 당신 쉬어라!</div>
+            <p>이불 덮고 귤 까먹으며 볼 만화 추천</p>
+            <div>
+              <img
+                src="img/book1.png"
+                alt="carousel-img"
+                className="inner-img"
+                width={10}
+                height={200}
+              />
+            </div>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item className="inner">
-          <img className="d-block w-100" src="img/6.png" alt="Third slide" />
+          <img
+            className="d-block w-100 car-img"
+            src="img/6.png"
+            alt="Third slide"
+          />
 
           <Carousel.Caption className="inner-text">
             <div>올해, 난 무엇을 했나 </div>
             <div>불안이 고개를 든다면</div>
             <p>침대와 한 몸인 사람을 위한 책 추천</p>
+            <div>
+              <img
+                src="img/find.png"
+                alt="carousel-img"
+                className="inner-img"
+              />
+            </div>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item className="inner">
-          <img className="d-block w-100" src="img/5.png" alt="Third slide" />
+          <img
+            className="d-block w-100 car-img"
+            src="img/5.png"
+            alt="Third slide"
+          />
 
           <Carousel.Caption className="inner-text">
             <div>에디터가 엄선한 </div>
             <div>11월 3주 베스트 셀러</div>
             <p>어떤 책을 읽을지 고민이라면</p>
+            <div>
+              <img
+                src="img/book2.png"
+                alt="carousel-img"
+                className="inner-img"
+              />
+            </div>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
@@ -57,24 +138,65 @@ function Today() {
           <div className="dragCard-title">한 달이내 출간된 책</div>
           <DragCarousel />
         </div>
+
+        <Row>
+          <Col lg={5} md={12}>
+            <div className="ad2">
+              {' '}
+              <div>
+                책을 통해 배우는 기쁨
+                <p>밀리 선정 베스트!</p>
+              </div>
+              <img src="img/book2.png" alt="back-img" />
+            </div>
+          </Col>
+          <Col lg={7} md={12}>
+            <div className="ad1">
+              {' '}
+              <div>
+                일상을 멋있게 만드는 다섯가지 독서법!
+                <p>독서로 가치있는 일상을 만들어보세요!</p>
+              </div>
+              <img src="img/add.png" alt="back-img" />
+            </div>
+          </Col>
+        </Row>
         <div className="dragCard">
           <div className="dragCard-title">지금 새로 들어온 책</div>
           <DragCarousel />
+          <DragCarousel />
         </div>
+
         <div className="dragCard">
           <div className="dragCard-title">이번주 취향별 추천책</div>
           <div>
-            {bookCategory.map(
-              category =>
-                category == { category } && (
-                  <Container>
-                    <Row>
-                      <Col></Col>
-                    </Row>
-                  </Container>
-                )
-            )}
+            {bookCategory.map(btn => (
+              <button key={btn} onClick={() => bookType(btn)}>
+                {btn}
+              </button>
+            ))}
           </div>
+
+          <div>
+            <Row>
+              {recommend.map(books => {
+                const { id, coverImg, title, author } = books;
+                return (
+                  <Col lg={4} sm={6} key={id} className="cardWrap">
+                    <div className="cardImg">
+                      <img src={coverImg} alt="coverImg" />
+                    </div>
+                    <div className="title">{title}</div>
+                    <div className="author">{author}</div>
+                  </Col>
+                );
+              })}
+            </Row>
+          </div>
+        </div>
+        <div className="dragCard">
+          <div className="dragCard-title">지금 새로 들어온 책</div>
+          <DragCarousel />
         </div>
       </div>
     </div>
